102482de87746ea91c35ce028bb6cef4c4b0f23b
minjaeju <53229953+minjaeju@users.noreply.github.com>
2020-01-14 11:18:52 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ def get_args():
 
     argp = argparse.ArgumentParser()
 
-    argp.add_argument('--transformer', action='store_true', default = False, help = 'transforemr')
+    argp.add_argument('--transformer', action='store_true', default = False, help = 'transformer')
     argp.add_argument('--batch_size', type = int, default = 32, help = 'batch_size')
 
     return argp.parse_args()
