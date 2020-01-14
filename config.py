484ce9942d28d703da482208512cd6228c8a1048
minjaeju <53229953+minjaeju@users.noreply.github.com>
2020-01-14 11:14:39 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import argparse
+
+def get_args():
+
+    argp = argparse.ArgumentParser()
+
+    argp.add_argument('--transformer', action='store_true', default = False, help = 'transforemr')
+    argp.add_argument('--batch_size', type = int, default = 32, help = 'batch_size')
+
+    return argp.parse_args()
